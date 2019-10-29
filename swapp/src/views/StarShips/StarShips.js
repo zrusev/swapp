d47adf87c56d0e642f612beeb06cd8856bee9073
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-10-29 15:15:41 +0200

    StarShips view and components; Nivo responsive radar chart

--- swapp/src/views/StarShips/StarShips.js
+++ swapp/src/views/StarShips/StarShips.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import {
+    Wrapper as RadarWrapper,
+    Radar,
+} from './components/_index';
+
+import {
     Container,
     Card,
     Header,
     Wrapper,
-    Line,
     Media,
     Details,
     Title,
@@ -15,60 +19,81 @@ import {
     Label,
 } from '../Episodes/Episode/component/_index';
 
-const StarShips = () => {
+const dummy = [
+  {
+    "metric": "cost_in_credits",
+    "value": "250",
+  },
+  {
+    "metric": "max_atmosphering_speed",
+    "value": "200",
+  },
+  {
+    "metric": "crew",
+    "value": "100",
+  },
+  {
+    "metric": "hyperdrive_rating",
+    "value": "180",
+  },
+  {
+    "metric": "MGLT",
+    "value": "30",
+  }
+];
 
-    return (
-        <>
-            <Header>
-                <h1>Jedi starfighter</h1>
-                <h3>(Delta-7 Aethersprite-class interceptor)</h3>
-            </Header>
-            <Container>
-                <Card>
-                    <Title>
-                        <h3>Jedi starfighter</h3>
-                    </Title>
-                    <Details>
-                        <Media>
-                            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/obi-wan-show-1567178968.jpg?crop=0.486xw:0.973xh;0.512xw,0.00340xh&resize=480:*" alt="" />
-                        </Media>
-                        <Label>Class:&nbsp;
-                            <span>
-                                Starfighter
-                            </span>
-                        </Label>
-                        <Label>Cost:&nbsp;
-                            <span>
-                                180000 credits
-                            </span>
-                        </Label>
-                        <Label>Crew:&nbsp;
-                            <span>
-                                1
-                            </span>
-                        </Label>
-                        <Label>Max Atmospheric Speed:&nbsp;
-                            <span>
-                                1153
-                            </span>
-                        </Label>
-                        <Label>Hyperdrive Rating:&nbsp;
-                            <span>
-                                1
-                            </span>
-                        </Label>
-                    </Details>
-                </Card>
-                <Wrapper>
-                    <Title>
-                        <h1>Compared to Starship Class Max</h1>
-                    </Title>
-                    <Line />
-
-                </Wrapper>
-            </Container>
-        </>
-    );
-};
+const StarShips = () => (
+    <>
+        <Header>
+            <h1>Jedi starfighter</h1>
+            <h3>(Delta-7 Aethersprite-class interceptor)</h3>
+        </Header>
+        <Container>
+            <Card>
+                <Title>
+                    <h3>Jedi starfighter</h3>
+                </Title>
+                <Details>
+                    <Media>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYvNE9TQzL-IG9aGe4CjZr_u6Tw2ErLvGlMSePWurIiGj8ZijbEA" alt="" />
+                    </Media>
+                    <Label>Class:&nbsp;
+                        <span>
+                            Starfighter
+                        </span>
+                    </Label>
+                    <Label>Cost:&nbsp;
+                        <span>
+                            180000 credits
+                        </span>
+                    </Label>
+                    <Label>Crew:&nbsp;
+                        <span>
+                            1
+                        </span>
+                    </Label>
+                    <Label>Max Atmospheric Speed:&nbsp;
+                        <span>
+                            1153
+                        </span>
+                    </Label>
+                    <Label>Hyperdrive Rating:&nbsp;
+                        <span>
+                            1
+                        </span>
+                    </Label>
+                </Details>
+            </Card>
+            <Wrapper>
+                <Title>
+                    <h1>Compared to Starship Class Max</h1>
+                </Title>
+                <RadarWrapper>
+                    <Radar data={dummy} />
+                </RadarWrapper>
+            </Wrapper>
+        </Container>
+    </>
+);
 
 export default StarShips;
